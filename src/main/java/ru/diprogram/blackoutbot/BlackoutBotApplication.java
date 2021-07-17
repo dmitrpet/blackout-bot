49d3ec94281e50070d625d27f2e1b2ea74bff026
dmitrpet <dmitrpet05@gmail.com>
2021-07-17 17:28:39 +0500

    Инициализация проекта

---
+++ src/main/java/ru/diprogram/blackoutbot/BlackoutBotApplication.java
@@ -0,0 +1,13 @@
+package ru.diprogram.blackoutbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackoutBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackoutBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/diprogram/blackoutbot/BlackoutBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.diprogram.blackoutbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackoutBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
